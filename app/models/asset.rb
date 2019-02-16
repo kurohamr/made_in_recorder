336a4a4e1cb7095764a0e53b45f47df3e3bdd263
legopo <legopo@legopo.local>
2019-02-16 16:00:21 +0900

    wip/投稿機能周りのバリデーション作成

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,4 +2,6 @@ class Asset < ApplicationRecord
   belongs_to :assetable, polymorphic: true
 
   mount_uploader :image, ImageUploader
+
+  validates :image, presence: true
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :post, foreign_key: :post_id
   has_one :address, as: :addressable
+
+  validates :name, presence: true, length: { in: 1..50 }
 end
