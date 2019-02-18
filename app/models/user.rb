a432cdd18617d5e30e78e52f82479dc8c1cfa104
legopo <legopo@legopo.local>
2019-02-18 19:23:37 +0900

    メールの確認機能を停止

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable #, :confirmable
 
   has_one :address, as: :addressable, dependent: :destroy
   has_one :asset, as: :assetable, dependent: :destroy
