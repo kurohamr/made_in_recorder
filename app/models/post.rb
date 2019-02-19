3b062a222e46e49bc0f4b8020c71ce90c3d87d61
legopo <legopo@legopo.local>
2019-02-19 15:39:50 +0900

    タグ関連のテーブル設計を変更し、アソシエーションを構築

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   accepts_nested_attributes_for :thing, allow_destroy: true
   accepts_nested_attributes_for :place, allow_destroy: true
   belongs_to :user
+  has_many :post_tags, dependent: :destroy
+  has_many :relation_tags, through: :post_labels, source: :label
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,4 @@
 class Tag < ApplicationRecord
-  has_many :place_tags, dependent: :destroy
-  has_many :thing_tags, dependent: :destroy
-  has_many :relation_posts, through: :place_tags, source: :post
-  has_many :relation_posts, through: :thing_tags, source: :post
+  has_many :post_tags, dependent: :destroy
+  has_many :relation_posts, through: :post_tags, source: :post
 end
