e300b868c2889a365f36f691a1c8586485ba6c01
legopo <legopo@legopo.local>
2019-02-19 13:34:03 +0900

    assetが保存できなくなる原因らしきコードを一旦コメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,13 @@ class ApplicationController < ActionController::Base
   #   edit_user_path(current_user.id)
   # end
 
-  def after_sign_in_path_for(resource)
-    if current_user.asset.present?
-      user_path(current_user.id)
-    else
-      edit_user_path(current_user.id)
-    end
-  end
+  # def after_sign_in_path_for(resource)
+  #   if current_user.asset.present?
+  #     user_path(current_user.id)
+  #   else
+  #     edit_user_path(current_user.id)
+  #   end
+  # end
 
   def after_sign_out_path_for(resource)
     new_user_session_path
