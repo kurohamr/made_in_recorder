3f0c9d0d0fc3ddd26544d35d0f61f78503e7551c
legopo <legopo@legopo.local>
2019-02-21 13:02:03 +0900

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-   
+
   # def after_sign_up_path_for(resource)
   #   edit_user_path(current_user.id)
   # end
@@ -25,5 +25,12 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :introduction])
+  end
+
+  def check_correct_user
+    if user_signed_in? && current_user.id != @user.id
+        flash[:notice] = "権限がありません"
+        redirect_to(root_path)
+    end
   end
 end
