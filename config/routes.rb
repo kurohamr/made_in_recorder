e8af43239a445d3a0f52fd6c551094ed0a0f2748
legopo <legopo@legopo.local>
2019-02-22 17:17:27 +0900

    ログイン有無でrootを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
 #TODO/ root to をセッション有無で分岐
-  root to: 'posts#index'
+  # root to: 'posts#index'
 
   devise_for :users #resources :usersより上に書く
+
+  devise_scope :user do
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+
   resources :users, only: [:edit, :update, :destroy, :show] #,:index]
   resources :posts
   get '/posts/hashtag/:name', to:'posts#hashtags'
