04ff20f5c9decfb030c6001b96c66431ee3f9a1f
legopo <legopo@legopo.local>
2019-02-27 10:37:16 +0900

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,17 @@ class User < ApplicationRecord
         return open("https://raw.githubusercontent.com/legopo/made_in_recorder/master/app/assets/images/noimage.png")
       end
   end
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
