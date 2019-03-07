b6635e8a5ad0667d0f961b73fc544004e609799a
legopo <legopo@legopo.local>
2019-03-07 17:06:17 +0900

    ajaxでfavを変更できるように

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    favorite = current_user.favorites.find_or_create_by(post_id: params[:post_id])
-    redirect_to post_path(id: params[:post_id]), notice: "#{favorite.post.user.name}さんの投稿をお気に入り登録しました"
+    @post = Post.find(params[:post_id])
+    @favorite = current_user.favorites.find_or_create_by(post_id: params[:post_id])
   end
 
   def destroy
     this_post_id = set_post_id
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to post_path(post_id: this_post_id), notice: "#{favorite.post.user.name}さんの投稿をお気に入り解除しました"
+    @post = Post.find(this_post_id)
+    @favorite = current_user.favorites.find_by(id: params[:id]).destroy
   end
 
   private
