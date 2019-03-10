296c68802069430aa14c19d0ca4a1d6721e4ef61
legopo <legopo@legopo.local>
2019-03-10 17:32:20 +0900

    add Post model's test

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    name { 'User1' }
+    email {'user1@example.com'}
+    password { 'password' }
+    latitude { 35.681236 }
+    longitude { 139.767125 }
+  end
+
+  factory :seconduser, class: User do
+    name { 'User2' }
+    email {'user2@example.com'}
+    password { 'password' }
+    latitude { 35.681236 }
+    longitude { 139.767125 }
+  end
+end
