71fef5baf25469da9e27c83ad64d0805107c0cb4
legopo <legopo@legopo.local>
2019-03-16 12:37:01 +0900

    Once don't use about routing and asset pre compile.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'about', to: 'abouts#index'
+  # get 'about', to: 'abouts#index'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
